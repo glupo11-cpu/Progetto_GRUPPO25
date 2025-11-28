ec124299be2f625bb6e9b6f5d65d0fbfdaed59ad
glupo11-cpu <g.lupo11@studenti.unisa.it>
2025-11-28 10:54:21 +0100

    Primo commit

---
+++ src/main/java/it/unisa/diem/is/gruppo25/SystemInfo.java
@@ -0,0 +1,13 @@
+package it.unisa.diem.is.gruppo25;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/* doesn't work with source level 1.8:
+module it.unisa.diem.is.gruppo25 {
+    requires javafx.controls;
+    exports it.unisa.diem.is.gruppo25;
+}
+*/
